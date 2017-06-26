3a4053acd9fa63ef2f06859fad7dd4c4b1fa2987
Eier <Eier@Eier-HP>
2017-06-26 23:25:46 +0200

    new committe

--- src/main/java/mo/rana/controller/HelloOpenshiftController.java
+++ src/main/java/mo/rana/controller/HelloOpenshiftController.java
@@ -1,12 +1,17 @@
 package mo.rana.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloOpenshiftController {
 	@GetMapping("/hello")
 	public String sayHello(){
 		return "hello world! openshift rock the world of programming";
+	}
+	@GetMapping("/uk")
+	public @ResponseBody String getjson(){
+		return "ukbies:iyasu:gebremsekel";
 	}
 
 }
