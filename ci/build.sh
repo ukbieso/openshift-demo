105f093e64de61d6a8f138040992d343a4a63bc2
ukbieso <ukbieso@gmail.com>
2018-03-30 23:18:37 +0200

    changed name of the jar file

--- ci/build.sh
+++ ci/build.sh
@@ -6,4 +6,4 @@ cd java-test
    mvn  clean package
 cd ..
 
-cp java-test/target/concourse-maven-cf-simple-0.0.1-SNAPSHOT.jar build-output/.
+cp java-test/target/git-openShift-0.0.1-SNAPSHOT.jar build-output/.
