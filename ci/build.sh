d758e1af61b9c3c63ff40d8a5ccefb76ca559e05
ukbieso <ukbieso@gmail.com>
2018-03-30 23:09:55 +0200

    file permission changed and resource type changed

--- ci/build.sh
+++ ci/build.sh
@@ -6,4 +6,4 @@ cd java-test
    mvn  clean package
 cd ..
 
-cp source-code/target/concourse-maven-cf-simple-0.0.1-SNAPSHOT.jar build-output/.
+cp test-java/target/concourse-maven-cf-simple-0.0.1-SNAPSHOT.jar build-output/.
