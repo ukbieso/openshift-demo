becb11b3d2fb6267b373b30693d3c1aaf506a156
ukbieso <ukbieso@gmail.com>
2018-04-02 14:30:17 +0200

    test

--- ci/sonar.sh
+++ ci/sonar.sh
@@ -2,5 +2,5 @@
 
 set -e -x
 cd java-test
-   mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package org.sonarsource.scanner.maven:sonar-maven-plugin:3.4.0.905:sonar -Dsonar.host.url=http://192.168.10.148:9000 -Dsonar.login=0c670bdfb9fa967ca2e5e23a280e087571474e76
+   
  
