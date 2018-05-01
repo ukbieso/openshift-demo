5f078011c2479655908cd6843299666521fc3169
ukbieso <ukbieso@gmail.com>
2018-05-01 10:41:48 +0200

    test

--- ci/build.sh
+++ ci/build.sh
@@ -3,7 +3,7 @@
 set -e -x
 
 cd java-test
-    mvn clean package org.jacoco:jacoco-maven-plugin:prepare-agent org.sonarsource.scanner.maven:sonar-maven-plugin:3.4.0.905:sonar
+mvn clean package
 
 cd ..
 cp -a java-test/. build-output/
