ead6a708dfb2ac0a755253143459632d5f7cf64b
ukbieso <ukbieso@gmail.com>
2018-05-01 10:00:47 +0200

    test

--- ci/build.sh
+++ ci/build.sh
@@ -3,7 +3,7 @@
 set -e -x
 
 cd java-test
-   mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package org.sonarsource.scanner.maven:sonar-maven-plugin:3.4.0.905:sonar -Dsonar.host.url=http://192.168.10.148:9000 -Dsonar.login=0c670bdfb9fa967ca2e5e23a280e087571474e76 -Dsonar.projectKey=mo.rana.openshift:git-openShift -Dsonar.sources=. -Dsonar.javascript.lcov.reportPaths=coverage/lcov.info
+    mvn clean package org.jacoco:jacoco-maven-plugin:prepare-agent package org.sonarsource.scanner.maven:sonar-maven-plugin:3.4.0.905:sonar -Dsonar.host.url=http://192.168.10.148:9000 -Dsonar.login=0c670bdfb9fa967ca2e5e23a280e087571474e76 -Dsonar.projectKey=mo.rana.openshift:git-openShift -Dsonar.sources=. -Dsonar.javascript.lcov.reportPaths=coverage/lcov.info
 
 cd ..
 cp -a java-test/. build-output
