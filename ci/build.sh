c2be9b8f2a21c4213cdaafd6a8ddb2e01a54985c
ukbieso <ukbieso@gmail.com>
2018-05-01 15:19:47 +0200

    test

--- ci/build.sh
+++ ci/build.sh
@@ -2,10 +2,10 @@
 
 set -e -x
 
-cd exam
+cd java-test
 mvn clean package
 
 cd ..
-cp -a exam/. sonarqube-analysis-input
+cp -a java-test/. sonarqube-analysis-input
 #sleep 1m
 #cp java-test/target/git-openShift-0.0.1-SNAPSHOT.jar build-output/.
