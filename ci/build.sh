f4d58178b3204ad0ded2edcbd62e0ba26ef0a12e
ukbieso <ukbieso@gmail.com>
2018-05-01 14:37:00 +0200

    2test2

--- ci/build.sh
+++ ci/build.sh
@@ -6,6 +6,6 @@ cd java-test
 mvn clean package
 
 cd ..
-cp -a java-test/ build-output/
+cp -a java-test build-output
 #sleep 1m
 #cp java-test/target/git-openShift-0.0.1-SNAPSHOT.jar build-output/.
